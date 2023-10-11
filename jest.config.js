00a8930d9b8e6b79eb1c47287c70dbc5de4a7650
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-11 18:33:41 +0200

    test pass og 100% coverage :smile:

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
+//Hadde glemt noe her, la til test for alle filer som inneholder dette osv. For bedre test-coverage. 
+
+module.exports  = {
+  preset: 'react-scripts',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js', 'src/**/*.jsx'],
+  testEnvironment: 'jest-environment-jsdom',
   }
